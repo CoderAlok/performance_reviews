e640fee9f2d1db2e0ff577dfb918bc48863660a6
Alok Das <alokdas4all@gmail.com>
2021-12-08 01:05:09 +0530

    Added body-parser, pug pacjkages

--- index.js
+++ index.js
@@ -2,9 +2,36 @@
 
 
 // Add all external modules
-const PORT = 6000;
-console.log(PORT);
-// App variebles 
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+
+
+// App variables 
+const app = express();
+const port = process.env.PORT || 6003;
+
 
 
 // App configurations
+
+
+
+
+
+
+// App ROUTES
+app.get('/user/login', (req, res) => {
+    res.send({name: "Alok"});
+});
+
+
+
+
+
+// Server ACTIVATION
+app.listen(port, () => {
+    console.log(`Listening to requests on https://localhost:${port}`);
+});
